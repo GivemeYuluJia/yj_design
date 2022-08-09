21b7b3e89ef04fe129a8f6dfec84d68ce4e33c5a
lzl <1434797171@qq.com>
2022-08-09 15:47:29 +0800

    feat: add Loading and AutoComplete Components

---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback } from "react";
+
+import classNames from "classnames";
+
+export type LoadingProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  type?: 'spin';
+};
+const Loading: React.FC<LoadingProps> = (props) => {
+  const { type, className = '', style = {} } = props;
+
+  const LoadingComponents = useCallback(() => {
+    if(type === 'spin') {
+      return (
+        <div className='spinLoading'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      );
+    }
+    return (
+      <div className='defaultLoading' />
+    )
+  }, [type]);
+
+  return (
+    <div style={style} className={classNames(className, 'loadingContainer')}>
+      <LoadingComponents />
+    </div>
+  )
+}
+
+export default Loading;
---
+++ src/hooks/useClickOutSide.tsx
@@ -0,0 +1,17 @@
+import React, { RefObject, useEffect } from "react";
+
+const useClickOutSide = (ref: RefObject<HTMLElement>, handler: Function) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if(!ref.current || ref.current.contains(event.target as HTMLElement)) {
+        return;
+      }
+      handler(event);
+    }
+    document.addEventListener('click', listener);
+    return () => {
+      document.removeEventListener('click', listener);
+    }
+  }, [ref, handler])
+}
+export default useClickOutSide;
